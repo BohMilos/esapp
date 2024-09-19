9e039c4eff7b400a57e0b1eeb37a679e2ac4a9d6
BohMilos <eduard.sedlacek25@gmail.com>
2024-09-19 11:15:55 +0200

    second commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+import  Container  from "@mui/material/Typography";
+
+export const metadata = { title: "404 | SnapZoška" };
+
+export default function About() {
+
+  return(
+    <Container>
+      <Typography>Stránka sa nenašla</Typography>
+    </Container>
+  );
+}
---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,15 @@
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+import  Container  from "@mui/material/Container";
+
+export const metadata = { title: "O mne | SnapZoška" };
+
+export default function About() {
+
+  return(
+    <Container>
+      <Typography>Obsah o mne</Typography>
+    </Container>
+  );
+}
