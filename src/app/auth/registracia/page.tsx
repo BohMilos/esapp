f90033f65d573b5262cb2aa5c326342691d4ffc4
BohMilos <eduard.sedlacek25@gmail.com>
2024-09-25 09:28:30 +0200

    treti commit

---
+++ src/app/auth/registracia/page.tsx
@@ -0,0 +1,15 @@
+// src/app/auth/registracia/page.tsx
+
+import Typography from "@mui/material/Typography";
+import  Container  from "@mui/material/Container";
+
+export const metadata = { title: "Registrácia | SnapZoška" };
+
+export default function SignUp() {
+
+  return(
+    <Container>
+      <Typography>Registrácia</Typography>
+    </Container>
+  );
+}
---
+++ src/app/prispevok/[id]/page.tsx
@@ -0,0 +1,15 @@
+// src/app/prispevok/[id]/page.tsx
+
+import Typography from "@mui/material/Typography";
+import  Container  from "@mui/material/Container";
+
+export const metadata = { title: "Detail príspevku | SnapZoška" };
+
+export default function PostDetail() {
+
+  return(
+    <Container>
+      <Typography>Detail príspevku</Typography>
+    </Container>
+  );
+}
