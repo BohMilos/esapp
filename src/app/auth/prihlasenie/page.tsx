1ed30e6668f2bd296d3282b293d8f2631d190645
bohmilos <eduard.sedlacek25@gmail.com>
2024-10-23 08:45:49 +0200

    init change

--- src/app/auth/prihlasenie/page.tsx
+++ src/app/auth/prihlasenie/page.tsx
@@ -1,7 +1,7 @@
 // src/app/auth/prihlasenie/page.tsx
 
-import Typography from "@mui/material/Typography";
-import  Container  from "@mui/material/Container";
+/*import Typography from "@mui/material/Typography";
+import  Container  from "@mui/material/Container";*/
 import NonAuthHomeView from "@/sections/NonAuthHomeView";
 
 export const metadata = { title: "Zoznam profilov | SnapZoška" };
